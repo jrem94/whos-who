c8b1c75520c0c470e639fc501d4799e9f16bf68f
Joshua Morris <jmorris252@students.sbts.edu>
2018-11-21 10:20:51 -0600

    Reset to 11/20/18

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
-import StyledApp from './StyledApp.js'
 import PictureFrame from './PictureFrame/PictureFrame.js'
 import Engraving from './Engraving/Engraving.js'
 import PageContent from './PageContent/PageContent.js'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: lightseagreen;
+`
 
 class App extends Component {
   render () {
--- src/components/StyledApp.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-const StyledApp = styled.div`
-  background-color: lightseagreen;
-`
-export default StyledApp
